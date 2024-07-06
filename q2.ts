388c1da111e640e6d234840dc506928eeb7c2b7e
Natwaa <Natwara_ch@cmu.ac.th>
2024-07-06 19:19:54 +0700

    finishhh

--- q2.ts
+++ q2.ts
@@ -1,9 +1,18 @@
 // define interface for Student object
 /* Your code here */
+interface Students {
+  name: string;
+  score: number;
+}
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
+function findTopNames(students : Students[]) {
   /* Your code here */
+  const topnames = students
+  .filter((students) => students.score > 8)
+  .map((students) => students.name);
+  
+  return topnames;
 }
 
 // assign interface/type to the student1 object properly
@@ -16,3 +25,5 @@ const students1 = [
 console.log(findTopNames(students1));
 
 module.exports = findTopNames;
+
+//ณัฐวรา ไชยสิทธิ์ 660610758
